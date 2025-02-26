4ea188a6553d2486cc060008791750eb66b24da0
matheus2049alves <mathii10costa@gmail.com>
2025-02-25 21:05:02 -0300

    add middleware to ensure JWT authentication for protected routes

---
+++ src/middlewares/ensureAuthenticated.js
@@ -0,0 +1,24 @@
+const {verify} = require('jsonwebtoken');
+const AppError = require('../utils/AppError');
+const authConfig = require('../configs/auth');
+
+function ensureAuthenticated(request, response, next) {
+
+  const authHeader = request.headers.authorization;
+
+  if(!authHeader){
+    throw new AppError("JWT token is missing", 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
+    request.user = {
+      id: decoded.sub
+    }
+    return next();
+  } catch {
+    throw new AppError("Invalid JWT token", 401);
+  }
+}
